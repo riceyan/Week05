a47fc19a7abc464f8bc051661c4e01f90e9c051a
riceyan <johndoe@example.com>
2023-05-18 10:17:48 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id21017809/week05/AnswerActivity2.java
@@ -0,0 +1,23 @@
+package sg.edu.rp.c346.id21017809.week05;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class AnswerActivity2 extends AppCompatActivity {
+
+    TextView tvAnswer2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_answer2);
+
+        tvAnswer2 = findViewById(R.id.textView2);
+        Intent intentReceived = getIntent();
+        String questionsSelected = intentReceived.getStringExtra("Question");
+        tvAnswer2.setText(questionsSelected + " answer is: Add a G in front of the first letter and you get Gone");
+    }
+}
